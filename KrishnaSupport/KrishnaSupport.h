ebcc2e9dd4c126c639ca92f8238487f938023675
Apple <apple@Apple.local>
2018-12-05 14:53:17 +0530

    upload

---
+++ KrishnaSupport/KrishnaSupport.h
@@ -0,0 +1,19 @@
+//
+//  KrishnaSupport.h
+//  KrishnaSupport
+//
+//  Created by Apple on 05/12/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KrishnaSupport.
+FOUNDATION_EXPORT double KrishnaSupportVersionNumber;
+
+//! Project version string for KrishnaSupport.
+FOUNDATION_EXPORT const unsigned char KrishnaSupportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KrishnaSupport/PublicHeader.h>
+
+
